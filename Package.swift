92d067435e52e50f274205fb3df0e303096c4e4e
Aslam <103982697+MAP-I@users.noreply.github.com>
2022-04-19 13:28:36 +0530

    chumma

--- Package.swift
+++ Package.swift
@@ -22,7 +22,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyPackage😎",
-            dependencies: []),
+            dependencies: [""],
+            path: "Sources",
+            resources: [.process("Sources/Media.xcassets")]
+        ),
         .testTarget(
             name: "MyPackage😎Tests",
             dependencies: ["MyPackage😎"]),
